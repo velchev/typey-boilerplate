ef855213ddbde6a442c7d71a9e21dd8b92d0e3bc
Nikolaev Tomov <nikolaev.github@yahoo.com>
2019-10-20 12:19:54 +0100

    Add and run prettier script

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -6,7 +6,11 @@ export interface HelloProps {
 }
 
 export class Hello extends React.PureComponent<HelloProps, {}> {
-  public render () {
-    return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+  public render() {
+    return (
+      <h1>
+        Hello from {this.props.compiler} and {this.props.framework}!
+      </h1>
+    );
   }
 }
