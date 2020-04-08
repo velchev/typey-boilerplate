aaf635320b604d43d8236608c51c9c3ebb7956cb
Nikolaev Tomov <nikolaev.github@yahoo.com>
2020-04-08 18:33:48 +0100

    Hooks

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,14 +1,43 @@
 import React from 'react';
+import classnames from 'classnames';
 
 export interface Props {
   compiler: string;
   framework: string;
 }
 
-const Hello: React.FunctionComponent<Props> = ({ compiler, framework }) => (
-  <h2>
-    Hello from {compiler} and {framework}!
-  </h2>
-);
+// const wait = async (delay: number) =>
+//   // eslint-disable-next-line no-undef
+//   new Promise(resolve => {
+//     return setTimeout(() => {
+//       resolve(console.log('yay'));
+//     }, delay);
+//   });
+
+// async function doRequests(time: number) {
+//   await wait(time);
+// }
+
+const Hello: React.FunctionComponent<Props> = ({ compiler, framework }) => {
+  const [isRed, setRed] = React.useState(false);
+
+  React.useEffect(() => {
+    setTimeout(() => setRed(!isRed), 5000);
+  }, [isRed]);
+
+  React.useEffect(() => {
+    return () => console.log('DONE');
+  }, []);
+
+  return (
+    <h2
+      className={classnames('black', {
+        red: isRed,
+      })}
+    >
+      Hello from {compiler} and {framework}!
+    </h2>
+  );
+};
 
 export default React.memo(Hello);
