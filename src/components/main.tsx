8330dda942125857dbe5eb9574d40b3dc6e9cdfb
Nikolaev Tomov <nikolaev.github@yahoo.com>
2020-05-16 16:21:59 +0100

    Install react-router, clean up

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { StoreState } from '^/types';
 
-export interface Props {
+import '^/index.scss';
+import LogoPath from '^images/logo.png';
+
+export interface StateProps {
+  loading: boolean;
+}
+
+export interface Props extends StateProps {
   children: React.ReactNode;
 }
 
-const Header: React.FunctionComponent = () => <header></header>;
+const Header: React.FunctionComponent = () => <header>...</header>;
+const Footer: React.FunctionComponent = () => <footer>...</footer>;
 
-const Footer: React.FunctionComponent = () => <footer></footer>;
-
-const Main: React.FunctionComponent<Props> = ({ children }) => (
+const Main: React.FunctionComponent<Props> = ({ loading, children }) => (
   <>
-    <Header />
-    {children}
-    <Footer />
+    {loading ? (
+      <img className="loading-icon" src={LogoPath} />
+    ) : (
+      <>
+        <Header />
+        {children}
+        <Footer />
+      </>
+    )}
   </>
 );
 
-export default React.memo(Main);
+const mapStateToProps = (state: StoreState): StateProps => ({
+  loading: state.app.loading,
+});
+
+export default connect(mapStateToProps)(React.memo(Main));
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
+import { Switch, Route } from 'react-router';
 
-import App from '^/components/app';
 import { history, store } from '^/store';
+import Main from '^/components/main';
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <App />
+      <Main>
+        <Switch>
+          <Route path="/example" render={() => <h2>Example</h2>} />
+
+          <Route
+            path="/"
+            render={() => <h2>Hello from Typescript and React!</h2>}
+          />
+        </Switch>
+      </Main>
     </ConnectedRouter>
   </Provider>,
   document.getElementById('app'),
