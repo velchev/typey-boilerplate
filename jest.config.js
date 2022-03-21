f17b79f4dc099bc8f704fe8fa58675a35d5f28ca
Nikolaev Tomov <nikolaev.github@yahoo.com>
2022-03-21 12:14:12 +0000

    Setup enzyme

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  verbose: true,
+  testMatch: ['<rootDir>/**/*(*.)+(test).+(tsx|ts)'],
+  setupFilesAfterEnv: ['<rootDir>src/setup-tests.ts'],
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
+  collectCoverage: true,
+  coverageDirectory: 'target/coverage',
+  collectCoverageFrom: ['src/**/*.tsx'],
 };
