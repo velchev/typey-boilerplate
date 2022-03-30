c37f26c65021ddd6142560bab2ecb8b449fc122b
Nikolaev Tomov <nikolaev.github@yahoo.com>
2022-03-30 17:22:15 +0100

    Fix BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 import { history, store } from '^/store';
 import Main from '^/components/main';
@@ -12,17 +12,15 @@ import Ping from '^/components/ping';
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Main>
-        <Routes>
-          <Route path="/ping">
-            <Ping />
-          </Route>
+      <BrowserRouter>
+        <Main>
+          <Routes>
+            <Route path="/ping" element={<Ping />} />
 
-          <Route path="/">
-            <Hello />
-          </Route>
-        </Routes>
-      </Main>
+            <Route path="/" element={<Hello />} />
+          </Routes>
+        </Main>
+      </BrowserRouter>
     </ConnectedRouter>
   </Provider>,
   document.getElementById('app'),
